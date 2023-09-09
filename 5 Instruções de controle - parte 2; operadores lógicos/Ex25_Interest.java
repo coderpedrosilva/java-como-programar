9e621191dde6eb57c7adf513cb991ea8f3d9d4bb
pedro <progerios@hotmail.com>
2023-09-09 14:32:07 -0300

    Example 25

---
+++ 5 Instruções de controle - parte 2; operadores lógicos/Ex25_Interest.java
@@ -0,0 +1,22 @@
+// Figura 5.6: Interest.java
+// Cálculos de juros compostos com for.
+
+public class Ex25_Interest {
+    public static void main(String[] args) {
+        double amount; // quantia em depósito ao fim de cada ano
+        double principal = 1000.0; // quantidade inicial antes dos juros
+        double rate = 0.05; // taxa de juros
+
+        // exibe cabeçalhos
+        System.out.printf("%sï%20sÿ %n", "Year", "Amount on deposit");
+
+        // calcula quantidade de depósito para cada um dos dez anosÿ
+        for (int year = 1; year <= 10; ++year) {
+            // calcula nova quantidade durante ano especificado ÿ
+            amount = principal * Math.pow(1.0 + rate, year);
+
+            // exibe o ano e a quantidade
+            System.out.printf("%4d%,20.2f%n", year, amount);
+        }
+    }
+} // fim da classe Interest
