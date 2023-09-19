e78ea4bb0090eb8812fd1164280b76cc082d1292
pedro <progerios@hotmail.com>
2023-09-18 21:20:35 -0300

    Example 36

---
+++ 6 Métodos - um exame mais profundo/Ex36_RandomIntegers.java
@@ -0,0 +1,23 @@
+
+// Figura 6.6: RandomIntegers.java
+// Inteiros aleatórios deslocados e escalonados.
+import java.security.SecureRandom; // o programa usa a classe SecureRandomÿ
+
+public class Ex36_RandomIntegers {
+    public static void main(String[] args) {
+        // o objeto randomNumbers produzirá números aleatórios segurosÿ
+        SecureRandom randomNumbers = new SecureRandom();
+
+        // faz o loop 20 vezes
+        for (int counter = 1; counter <= 20; counter++) {
+            // seleciona o inteiro aleatório entre 1 e 6
+            int face = 1 + randomNumbers.nextInt(6);
+
+            System.out.printf("%d ", face); // exibe o valor gerado
+
+            // se o contador for divisível por 5, inicia uma nova linha de saída
+            if (counter % 5 == 0)
+                System.out.println();
+        }
+    }
+} // fim da classe RandomIntegers
