d9cef21d2422fdc5034a87e603f1cb600a188ff8
pedro <progerios@hotmail.com>
2023-11-01 21:13:07 -0300

    Update Example 81

--- 8 Classes e objetos - um exame mais profundo/Ex81_Interest.java
+++ 8 Classes e objetos - um exame mais profundo/Ex81_Interest.java
@@ -6,16 +6,16 @@ import java.text.NumberFormat;
 
 public class Interest {
     public static void main(String args[]) {
-        // quantidade principal inicial antes dos juros ÿ
+        // quantidade principal inicial antes dos juros
         BigDecimal principal = BigDecimal.valueOf(1000.0);
-        BigDecimal rate = BigDecimal.valueOf(0.05); // taxa de jurosÿ
+        BigDecimal rate = BigDecimal.valueOf(0.05); // taxa de juros
 
         // exibe cabeçalhos
         System.out.printf("%s%20s%n", "Year", "Amount on deposit");
 
         // calcula quantidade de depósito para cada um dos dez anos
         for (int year = 1; year <= 10; year++) {
-            // calcula nova quantidade durante ano especificado ÿ
+            // calcula nova quantidade durante ano especificado
             BigDecimal amount = principal.multiply(rate.add(BigDecimal.ONE).pow(year));
 
             // exibe o ano e a quantidade
