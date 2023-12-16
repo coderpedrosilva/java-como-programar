9e8370f523abcd3781448fc0cba45cd47bbf1794
pedro <progerios@hotmail.com>
2023-12-15 21:38:51 -0300

    Example 124

---
+++ 12 Componentes GUI - parte 1/Ex124_ComboBoxTest.java
@@ -0,0 +1,13 @@
+
+// Figura 12.22: ComboBoxTest.java
+// Testando ComboBoxFrame.
+import javax.swing.JFrame;
+
+public class Ex124_ComboBoxTest {
+    public static void main(String[] args) {
+        Ex123_ComboBoxFrame comboBoxFrame = new Ex123_ComboBoxFrame();
+        comboBoxFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        comboBoxFrame.setSize(350, 150);
+        comboBoxFrame.setVisible(true);
+    }
+} // fim da classe ComboBoxTest
