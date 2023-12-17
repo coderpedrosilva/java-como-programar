8597ec27b073f8816b17e0330897e4abb7a1dae4
pedro <progerios@hotmail.com>
2023-12-17 11:17:13 -0300

    Example 126

---
+++ 12 Componentes GUI - parte 1/Ex126_ListTest.java
@@ -0,0 +1,13 @@
+
+// Figura 12.24: ListTest.java
+// Selecionando as cores de uma JList.
+import javax.swing.JFrame;
+
+public class Ex126_ListTest {
+    public static void main(String[] args) {
+        Ex125_ListFrame listFrame = new Ex125_ListFrame(); // cria ListFrame
+        listFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        listFrame.setSize(350, 150);
+        listFrame.setVisible(true);
+    }
+} // fim da classe ListTest
