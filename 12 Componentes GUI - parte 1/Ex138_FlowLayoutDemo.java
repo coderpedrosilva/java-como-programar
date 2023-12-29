2e1f0c3a87f62e45f5d689762622255979d5cce4
pedro <progerios@hotmail.com>
2023-12-29 19:10:58 -0300

    Example 138

---
+++ 12 Componentes GUI - parte 1/Ex138_FlowLayoutDemo.java
@@ -0,0 +1,13 @@
+
+// Figura 12.40: FlowLayoutDemo.java
+// Testando FlowLayoutFrame.
+import javax.swing.JFrame;
+
+public class Ex138_FlowLayoutDemo {
+    public static void main(String[] args) {
+        Ex137_FlowLayoutFrame flowLayoutFrame = new Ex137_FlowLayoutFrame();
+        flowLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        flowLayoutFrame.setSize(300, 75);
+        flowLayoutFrame.setVisible(true);
+    }
+} // fim da classe FlowLayoutDemo
