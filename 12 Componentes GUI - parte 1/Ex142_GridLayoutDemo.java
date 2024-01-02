76905fbac83e8f041b981b60de758f2b2e824523
pedro <progerios@hotmail.com>
2024-01-02 20:04:04 -0300

    Example 142

---
+++ 12 Componentes GUI - parte 1/Ex142_GridLayoutDemo.java
@@ -0,0 +1,13 @@
+
+// Figura 12.44: GridLayoutDemo.java
+// Testando GridLayoutFrame.
+import javax.swing.JFrame;
+
+public class Ex142_GridLayoutDemo {
+    public static void main(String[] args) {
+        Ex141_GridLayoutFrame gridLayoutFrame = new Ex141_GridLayoutFrame();
+        gridLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gridLayoutFrame.setSize(300, 200);
+        gridLayoutFrame.setVisible(true);
+    }
+} // fim da classe GridLayoutDemo
