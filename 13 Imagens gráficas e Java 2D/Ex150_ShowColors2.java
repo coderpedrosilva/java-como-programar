3179fb424465126b660cbb147a0f6e44d219fb9d
pedro <progerios@hotmail.com>
2024-01-10 10:02:34 -0300

    Example 150

---
+++ 13 Imagens gráficas e Java 2D/Ex150_ShowColors2.java
@@ -0,0 +1,12 @@
+
+// Figura 13.8: ShowColors2.java
+// Escolhendo cores com JColorChooser.
+import javax.swing.JFrame;
+
+public class Ex150_ShowColors2 {
+    // executa o aplicativo
+    public static void main(String[] args) {
+        Ex149_ShowColors2JFrame application = new Ex149_ShowColors2JFrame();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+} // fim da classe ShowColors2
