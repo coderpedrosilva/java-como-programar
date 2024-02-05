a76a14089a998ff0169e1ad4a5416b048aae7085
pedro <coderpedrosilva@gmail.com>
2024-02-05 19:49:45 -0300

    Example 170

---
+++ 14 Strings, caracteres e expressões regulares/Ex170_SubString.java
@@ -0,0 +1,15 @@
+// Figura 14.6: SubString.java
+// métodos substring da classe String.
+
+public class Ex170_SubString {
+    public static void main(String[] args) {
+        String letters = "abcdefghijklmabcdefghijklm";
+
+        // testa métodos substring
+        System.out.printf("Substring from index 20 to end is \"%s\"%n",
+                letters.substring(20));
+        System.out.printf("%s \"%s\"%n",
+                "Substring from index 3 up to, but not including 6 is",
+                letters.substring(3, 6));
+    }
+} // fim da classe Substring
