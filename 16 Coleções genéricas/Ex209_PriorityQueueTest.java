3c7093a7605e8d2dcff3adf170d3bdcd03e69c77
pedro <coderpedrosilva@gmail.com>
2024-03-15 20:18:54 -0300

    Example 209

---
+++ 16 Coleções genéricas/Ex209_PriorityQueueTest.java
@@ -0,0 +1,24 @@
+
+// Figura 16.15: PriorityQueueTest.java
+// Programa de teste de PriorityQueue.
+import java.util.PriorityQueue;
+
+public class Ex208_PriorityQueueTest {
+    public static void main(String[] args) {
+        // fila de capacidade 11
+        PriorityQueue<Double> queue = new PriorityQueue<>();
+
+        // insere elementos na fila
+        queue.offer(3.2);
+        queue.offer(9.8);
+        queue.offer(5.4);
+
+        System.out.print("Polling from queue: ");
+
+        // exibe elementos na fila
+        while (queue.size() > 0) {
+            System.out.printf("%.1f ", queue.peek()); // visualiza o elemento superior
+            queue.poll(); // remove o elemento superior
+        }
+    }
+} // fim da classe PriorityQueueTest
